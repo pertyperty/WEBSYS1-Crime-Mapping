bccbf943ec1ddc49389ccd5f0f665f1b20de44ae
phertphert <20244042@s.ubaguio.edu>
2026-05-07 13:01:02 +0800

    Update #24

--- Crime-mapping/public/about.php
+++ Crime-mapping/public/about.php
@@ -18,7 +18,7 @@ init_secure_session();
     <div class="page-shell">
         <header class="site-header">
             <div class="brand">
-                <span class="brand-mark"></span>
+                <img class="brand-logo" src="../assets/images/logo/la-trinidad.png" alt="La Trinidad" />
                 <div>
                     <div class="brand-title">La Trinidad Crime Mapping</div>
                     <div class="brand-subtitle">About the project</div>
